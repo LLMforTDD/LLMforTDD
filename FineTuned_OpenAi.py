562f1be24e87aeb20d0a40436b0febc294a70ae9
LLMforTDD <151166394+LLMforTDD@users.noreply.github.com>
2023-12-21 09:09:30 +1100

    Update FineTuned_OpenAi.py

--- FineTuned_OpenAi.py
+++ FineTuned_OpenAi.py
@@ -1,32 +1,89 @@
-import openai
+import pandas as pd
 import pickle
-import openai
 import json
+import openai
+import time
+from tqdm import tqdm
+import os.path
+from tenacity import ( retry, stop_after_attempt, wait_random_exponential ) # for exponential backoff
+import argparse
 
-def generate_junit_test_case(description):
-    # Set your OpenAI API key here
-    openai.api_key = ""
+parser.add_argument('--train', '-t', type=str, help='The path to the train TSV file.')
+parser.add_argument('--test', '-s', type=str, help='The path to the test TSV file.')
+args = parser.parse_args()
 
-    # Construct the prompt
-    prompt = f"Create a JUnit test case for the following requirement:\n{description}"
+train_file = args.train
+test_file = args.test
 
-    try:
-        # Generate the test case using OpenAI's GPT-4
-        response = openai.Completion.create(
-            engine="text-davinci-004",  # You might need to change this based on available models
-            prompt=prompt,
-            max_tokens=150  # You can adjust this based on how lengthy the expected output is
-        )
+input_column = 'Description'
+output_column = 'test_case'
 
-        # Extracting the text from the response
-        test_case = response.choices[0].text.strip()
-        return test_case
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+train_dataset_path = train_file
+test_dataset_path = test_file
 
-# Example usage
-description = "A Calculator class with a method add(int a, int b) that returns the sum of a and b."
-test_case = generate_junit_test_case(description)
-if test_case:
-    print("Generated JUnit Test Case:\n", test_case)
+# Reading the train and test files
+df_train = pd.read_csv(train_dataset_path,delimiter='\t')
+df_test = pd.read_csv(test_dataset_path,delimiter='\t')
+
+#Setting up the fine-tuning parameters
+fine_tuning_data = []
+
+for i in tqdm(range(len(df_train))):
+    description_content = df_train[input_column][i]
+    test_case = df_train[output_column][i]
+    fine_tuning_data.append({
+        "prompt": description_content,
+        "completion": test_case
+    })
+
+# Fine-tuning the GPT-3.5 model
+openai.api_key = json.load(open('access_token.json'))['openai_access_token']
+
+fine_tuning_response = openai.CreateFineTuneJob.create(
+    training_set=fine_tuning_data,
+    model="gpt-3.5-turbo"
+)
+
+# Waiting for the fine-tuning job to complete
+fine_tuning_job = openai.FineTuneJob.retrieve(fine_tuning_response["id"])
+while fine_tuning_job["status"] != "completed":
+    time.sleep(60)
+    fine_tuning_job = openai.FineTuneJob.retrieve(fine_tuning_response["id"])
+
+# Using the fine-tuned model to generate test cases
+@retry(wait=wait_random_exponential(min=0.3, max=2), stop=stop_after_attempt(6))
+def completion_with_backoff(**kwargs):
+    return openai.ChatCompletion.create(**kwargs)
+
+openai.api_key = key
+openai_completions = []
+curr_idx = 0
+skip = False
+if os.path.isfile('save/openai_gpt35_apps_testcase.json'):
+    skip = True
+    rfile = open('save/openai_gpt35_apps_testcase.json', "r")
+    curr_idx = len(rfile.readlines())
+    rfile.close()
+file = open('save/openai_gpt35_apps_testcase.json', "a")
+for i in tqdm(range(len(df_test))):
+    if i < curr_idx:
+        continue
+    if skip:
+        print(f'continue at idx: {i}')
+        skip = False
+        description_content = "Write a JUnit Test Case for the description provided : " + df_test[input_column][i]
+    completion = completion_with_backoff(
+        model=fine_tuning_job["engine"],
+        messages=[
+            {"role": "user", "content": description_content}
+        ],
+        max_tokens=300
+    )
+    openai_completions.append(completion)
+    save_obj = {'idx': i, 'response': completion}
+    json_str = json.dumps(save_obj)
+    file.write(json_str + '\n')
+    time.sleep(1)
+file.close()
+with open('save/openai_gpt35_apps_testcase.pkl', 'wb') as f:
+    pickle.dump(openai_completions, f)
