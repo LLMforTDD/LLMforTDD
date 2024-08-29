76e6f8a32acb86fef6ef50e98ba8b07515301362
LLMforTDD <151166394+LLMforTDD@users.noreply.github.com>
2024-08-29 10:29:09 +1000

    LLM

---
+++ InCoder_sample.py
@@ -0,0 +1,267 @@
+from typing import List
+import os
+import torch
+import tokenizers
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import json
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+tokenizers_version = tuple(int(n) for n in tokenizers.__version__.split('.'))
+if tokenizers_version < (0, 12, 1):
+    print("warning: Your tokenizers version looks old and you will likely have formatting issues. We recommend installing tokenizers >= 0.12.1")
+
+# set BIG_MODEL to use the 6.7B parameter model
+BIG_MODEL = True
+
+# use a GPU
+CUDA = True
+
+# print intermediate outputs of infilling
+VERBOSE = False
+
+if BIG_MODEL:
+    model_name = "facebook/incoder-6B"
+
+    # the arguments added below will load a half precision version of the model,
+    # which requires less RAM than loading the full float32 version.  this 
+    # should fit in ~16GB of RAM
+    # NOTE: half precision should *not* be used if you plan to fine-tune the
+    # model. You'll need full precision and a lot of GPU memory. We have not
+    # tested fine-tuning in `transformers` (the model was trained in fairseq)
+    if CUDA:
+        kwargs = dict(
+            revision="float16", 
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True,
+        )
+    else:
+        kwargs = dict(
+            low_cpu_mem_usage=True,
+        )
+else:
+    model_name = "facebook/incoder-1B"
+    kwargs = {}
+
+print("loading model")
+model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir='/HDD18TB/wannita/LLM_HF', **kwargs)
+print("loading tokenizer")
+tokenizer = AutoTokenizer.from_pretrained(model_name,  cache_dir='/HDD18TB/wannita/LLM_HF')
+print("loading complete")
+
+if CUDA:
+    # if you plan to fine-tune the model, you should not use half precision.
+    model = model.half().cuda()
+
+# signals the start of a document
+BOS = "<|endoftext|>"
+# signals the end of a generated infill
+EOM = "<|endofmask|>"
+
+def make_sentinel(i):
+    # signals (1) a location to insert an infill and (2) the start of the infill generation
+    return f"<|mask:{i}|>"
+
+def generate(input: str, max_to_generate: int=128, temperature: float=0.2):
+    """
+    Do standard left-to-right completion of the prefix `input` by sampling from the model
+    """
+    input_ids = tokenizer(input, return_tensors="pt").input_ids
+    if CUDA:
+        input_ids = input_ids.cuda()
+    max_length = max_to_generate + input_ids.flatten().size(0)
+    if max_length > 2048:
+        print("warning: max_length {} is greater than the context window {}".format(max_length, 2048))
+    with torch.no_grad():
+        output = model.generate(input_ids=input_ids, do_sample=True, top_p=0.95, temperature=temperature, max_length=max_length)
+    # pass clean_up_tokenization_spaces=False to avoid removing spaces before punctuation, e.g. "from ." -> "from."
+    detok_hypo_str = tokenizer.decode(output.flatten(), clean_up_tokenization_spaces=False)
+    if detok_hypo_str.startswith(BOS):
+        detok_hypo_str = detok_hypo_str[len(BOS):]
+    return detok_hypo_str
+
+def infill(parts: List[str], max_to_generate: int=128, temperature: float=0.2, extra_sentinel: bool=True, max_retries: int=1):
+    """
+    Generate infills to complete a partial document, e.g.
+    [A C E] -> [A B C D E], where B and D are infills that have been generated.
+
+    parts: List[str]. list of parts of the document. One string will be
+            inserted in between each element, i.e. infilling N-1 locations for a list
+            of length N.
+    max_to_generate: int. maximum number of tokens to generate. Keep in mind
+            that the model context size is 2048.
+    temperature: float. temperature parameter for sampling.
+    extra_sentinel: bool. we recommend setting this to True, as it makes it
+            easier for the model to end generated infills. See the footnote in 
+            section 2.2 of our paper for details.
+    max_retries: int. if > 1, use rejection sampling to keep sampling infills until
+            all infills sample a completion token.
+
+    returns a dictionary containing the following:
+        text:  str, the completed document (with infills inserted)
+        parts:  List[str], length N. Same as passed to the method
+        infills:  List[str], length N-1. The list of infills generated
+        retries_attempted:  number of retries used (if max_retries > 1)
+    """
+    assert isinstance(parts, list)
+    retries_attempted = 0
+    done = False
+
+    while (not done) and (retries_attempted < max_retries):
+        retries_attempted += 1
+
+        if VERBOSE:
+            print(f"retry {retries_attempted}")
+        
+        ## (1) build the prompt
+        if len(parts) == 1:
+            prompt = parts[0]
+        else:
+            prompt = ""
+            # encode parts separated by sentinel
+            for sentinel_ix, part in enumerate(parts):
+                prompt += part
+                if extra_sentinel or (sentinel_ix < len(parts) - 1):
+                    prompt += make_sentinel(sentinel_ix)
+        
+        infills = []
+        complete = []
+
+        done = True
+
+        ## (2) generate infills
+        for sentinel_ix, part in enumerate(parts[:-1]):
+            complete.append(part)
+            prompt += make_sentinel(sentinel_ix)
+            # TODO: this is inefficient as it requires re-encoding prefixes repeatedly
+            completion = generate(prompt, max_to_generate, temperature)
+            completion = completion[len(prompt):]
+            if EOM not in completion:
+                if VERBOSE:
+                    print(f"warning: {EOM} not found")
+                completion += EOM
+                done = False
+            completion = completion[:completion.index(EOM) + len(EOM)]
+            infilled = completion[:-len(EOM)]
+            infills.append(infilled)
+            complete.append(infilled)
+            prompt += completion
+        complete.append(parts[-1])
+        text = ''.join(complete)
+
+    if VERBOSE:
+        print("generated text:")
+        print(prompt)
+        print()
+        print("parts:")
+        print(parts)
+        print()
+        print("infills:")
+        print(infills)
+        print()
+        print("restitched text:")
+        print(text)
+        print()
+    
+    return {
+        'text': text, # str, the completed document (with infills inserted)
+        'parts': parts, # List[str], length N. Same as passed to the method
+        'infills': infills, # List[str], length N-1. The list of infills generated
+        'retries_attempted': retries_attempted, # number of retries used (if max_retries > 1)
+    } 
+
+def code_to_docstring(max_to_generate=128, temperature=0.2):
+    # this will sometimes generate extra functions! this can be avoided by truncating generation when e.g. a """ is produced
+    example = '''\
+def count_words(filename):
+    """ <insert> """
+    counts = Counter()
+    with open(filename) as file:
+        for line in file:
+            words = line.split(' ')
+            counts.update(words)
+    return counts'''
+
+    parts = example.split("<insert>")
+    result = infill(parts, max_to_generate=max_to_generate, temperature=temperature)
+    print("completed document:")
+    print(result["text"])
+    return result["text"]
+
+def docstring_to_code(max_to_generate=128, temperature=0.2):
+    example = '''\
+def <insert>
+    """ Count the number of occurrences of each word in the file. """
+    <insert>
+<|/ file |>'''
+    parts = example.split("<insert>")
+    result = infill(parts, max_to_generate=max_to_generate, temperature=temperature)
+    print("completed document:")
+    print(result["text"])
+    return result["text"]
+
+def text_to_testcase(max_to_generate=128, temperature=0.2):
+    example = '''\
+import sys
+import io
+def test_call_solution(input_string):
+    sys.stdin = io.StringIO(input_string)
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    call_solution()
+    sys.stdin = sys.__stdin__
+    sys.stdout = sys.__stdout__
+    actual_output = captured_output.getvalue().strip()
+    return actual_output
+def call_solution():
+    r"""
+    An accordion is a string (yes, in the real world accordions are musical instruments, but let's forget about it for a while) which can be represented as a concatenation of: an opening bracket (ASCII code $091$), a colon (ASCII code $058$), some (possibly zero) vertical line characters (ASCII code $124$), another colon, and a closing bracket (ASCII code $093$). The length of the accordion is the number of characters in it.
+    
+    For example, [::], [:||:] and [:|||:] are accordions having length $4$, $6$ and $7$. (:|:), {:||:}, [:], ]:||:[ are not accordions. 
+    
+    You are given a string $s$. You want to transform it into an accordion by removing some (possibly zero) characters from it. Note that you may not insert new characters or reorder existing ones. Is it possible to obtain an accordion by removing characters from $s$, and if so, what is the maximum possible length of the result?
+    
+    
+    -----Input-----
+    
+    The only line contains one string $s$ ($1 \le |s| \le 500000$). It consists of lowercase Latin letters and characters [, ], : and |.
+    
+    
+    -----Output-----
+    
+    If it is not possible to obtain an accordion by removing some characters from $s$, print $-1$. Otherwise print maximum possible length of the resulting accordion.
+    
+    
+    -----Examples-----
+    Input
+    |[a:b:|]
+    
+    Output
+    4
+    
+    Input
+    |]:[|:]
+    
+    Output
+    -1
+    """
+    pass
+
+# check the correctness of the `call_solution` function
+assert <insert>
+assert <insert>
+assert <insert>
+'''
+    parts = example.split("<insert>")
+    result = infill(parts, max_to_generate=max_to_generate, temperature=temperature)
+    print("completed document:")
+    print(result["text"])
+    return result["text"]
+
+if __name__ == "__main__":
+    print("code to docstring test:")
+    code_to_docstring()
+    print()
+    print("docstring to code test:")
+    docstring_to_code()
+    print()
+    print("text to testcase:")
+    text_to_testcase()
---
+++ finetuning_apps_codeLlama.py
@@ -0,0 +1,136 @@
+import pandas as pd
+import numpy as np
+import os
+import logging
+import json
+import torch
+import argparse
+from transformers import RobertaTokenizer, get_scheduler
+from sklearn.model_selection import train_test_split
+from transformers import AutoConfig, AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM, \
+    GPT2Config, GPT2Model, RobertaModel, PLBartForCausalLM, \
+    Trainer, TrainingArguments, \
+    DataCollatorForSeq2Seq, DataCollatorForLanguageModeling, T5ForConditionalGeneration
+
+# Define the token
+token = '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'
+
+# Initialize the CodeLlama model and tokenizer
+checkpoint = "codellama/CodeLlama-small"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, revision="main", token=token)
+model = AutoModelForCausalLM.from_pretrained(checkpoint, revision="main", token=token)
+
+parser = argparse.ArgumentParser(description='Description of your script.')
+parser.add_argument('--input', '-i', type=str, help='The path to the TSV file.')
+args = parser.parse_args()
+
+input_file = args.input
+
+input_dir = '/home/saranya/HDD18TB/RL/PyRL-text-to-testcase-main/Dataset/Test/CSVFormat.csv'
+input_column = 'description'
+output_filename = '/home/saranya/HDD18TB/RL/PyRL-text-to-testcase-main/Output/Csv'  # Specify your output directory
+output_suffix = '_testcase'
+batch_size = 1
+
+# Configure CUDA settings
+device = "cuda"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint,  cache_dir='/HDD18TB/saranya/LLM_HF')
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = 'left'
+tokenizer.truncation_side = 'left'
+model = AutoModelForCausalLM.from_pretrained(checkpoint,
+                                             device_map="auto",
+                                             load_in_8bit=True,
+                                             cache_dir='/HDD18TB/saranya/LLM_HF')
+
+model_name = 'codellama/CodeLlama-small'
+is_shuffle = True
+
+train_dataset_path = input_file
+output_column = 'test_case'
+output_dir = 'save/APPS/codellama/CodeLlama-small'
+
+# Set environment variables
+os.environ['TRANSFORMERS_NO_ADVISORY_WARNINGS'] = 'true'
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+# Set logging
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+log_file = output_dir + '/train.log'
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=log_file, filemode='a', format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
+logger.addHandler(logging.StreamHandler())
+logger.info(f"Is CUDA available: {torch.cuda.is_available()}")
+logger.info(f"CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+
+# Load the CSV
+train_df = pd.read_csv(train_dataset_path, delimiter='\t', error_bad_lines=False, skip_blank_lines=True)
+train_df, eval_df = train_test_split(train_df, test_size=0.1, random_state=42, shuffle=is_shuffle)
+
+# Convert the tokenized inputs and outputs into a PyTorch dataset
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def __getitem__(self, idx):
+        return {
+            "input_ids": torch.tensor(self.inputs["input_ids"][idx]),
+            "attention_mask": torch.tensor(self.inputs["attention_mask"][idx]),
+            "labels": torch.tensor(self.outputs["input_ids"][idx]),
+        }
+
+    def __len__(self):
+        return len(self.inputs["input_ids"])
+
+# Tokenize the inputs and outputs
+train_inputs = tokenizer(list(train_df[input_column]), padding=True, truncation=True, max_length=512)
+train_outputs = tokenizer(list(train_df[output_column]), padding=True, truncation=True, max_length=512)
+train_dataset = MyDataset(train_inputs, train_outputs)
+
+eval_inputs = tokenizer(list(eval_df[input_column]), padding=True, truncation=True, max_length=512)
+eval_outputs = tokenizer(list(eval_df[output_column]), padding=True, truncation=True, max_length=512)
+eval_dataset = MyDataset(eval_inputs, eval_outputs)
+logger.info('loaded dataset successfully.')
+
+training_args = TrainingArguments(
+    output_dir=output_dir,
+    learning_rate=2e-5,
+    weight_decay=0.01,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    gradient_accumulation_steps=4,
+    num_train_epochs=20,
+    logging_strategy="steps",
+    logging_steps=500,
+    save_strategy="epoch",
+    eval_accumulation_steps=1,
+    evaluation_strategy="epoch",
+    warmup_steps=1000,
+    fp16=True,
+    save_total_limit=10,
+    optim="adamw_torch",
+    lr_scheduler_type = "inverse_sqrt"
+)
+logger.info(f'''training_args: lr={training_args.learning_rate},
+            batch_size={training_args.per_device_train_batch_size},
+            epoch={training_args.num_train_epochs},
+            gradient_accumulation_steps={training_args.gradient_accumulation_steps},
+            warmup_steps={training_args.warmup_steps},
+            weight_decay={training_args.weight_decay},
+            optim={training_args.optim},
+            lr_scheduler_type={training_args.lr_scheduler_type},
+            fp16={training_args.fp16}
+            ''')
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    data_collator=DataCollatorForSeq2Seq(tokenizer) if 'CodeLlama' in model_name else DataCollatorForLanguageModeling(tokenizer, mlm=False),
+    tokenizer=tokenizer,
+)
+
+trainer.train()
